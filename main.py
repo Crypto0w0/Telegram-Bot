2b08af6e9538ba18f1984791ea6e9a3700ab5ffc
Alex Untilov <93001397+Crypto0w0@users.noreply.github.com>
2022-08-03 21:49:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import requests
 from aiogram import Bot, Dispatcher, types, executor
 
-WHEATHER_API_KEY = 'e70b3d12e10d1735e93d7770e126a258'
-
 bot = Bot(token = TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q=Odessa&appid={WHEATHER_API_KEY}&units=metric')
